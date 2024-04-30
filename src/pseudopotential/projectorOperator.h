187ff50df935a7b98731abc718566357f244b5c9
Moritz Gubler <moritz.gubler@gmail.com>
2024-04-30 16:45:34 +0200

    add constructor

--- src/pseudopotential/projectorOperator.h
+++ src/pseudopotential/projectorOperator.h
@@ -11,9 +11,27 @@ class Projector: public mrchem::RankZeroOperator {
 
     std::vector<PseudopotentialData> pp;
     std::vector<Projector> proj;
+    double prec;
 
 public:
-    Projector(mrchem::Molecule molecule, std::vector<PseudopotentialData> pp){
+    Projector(mrchem::Molecule molecule, std::vector<PseudopotentialData> pp, double prec){
+
+        this->pp = pp;
+        this->prec = prec;
+
+        // loop over all atoms and create projectors
+        for (int i = 0; i < molecule.getNNuclei() i++) {
+            Eigen::Vector3d pos = molecule.getNucleus(i).getPos();
+            for (int l = 0; l < pp[i].nProjectors; l++) {
+                for (int m = -l; m <= l; m++) {
+                    for (int isep = 0; isep < pp[i].nsep; isep++){
+                        proj.push_back(Projector(pos, pp[i].rl[l], isep, l, m, prec));
+                    }
+                    
+                }
+            }
+        }
+
     }
 
 mrchem::Orbital apply(mrchem::Orbital phi_p) {
