0c197c738f35cdf5d8dc1f101e9e4dc3885e7e10
Moritz Gubler <moritz.gubler@gmail.com>
2024-04-30 17:39:50 +0200

    fix silly errors

--- src/pseudopotential/projectorOperator.h
+++ src/pseudopotential/projectorOperator.h
@@ -7,25 +7,25 @@
 #include "qmfunctions/Orbital.h"
 #include "chemistry/Molecule.h"
 
-class Projector: public mrchem::RankZeroOperator {
+class ProjectorOperator: public mrchem::RankZeroOperator {
 
     std::vector<PseudopotentialData> pp;
-    std::vector<Projector> proj;
+    std::vector<ProjectorFunction> proj;
     double prec;
 
 public:
-    Projector(mrchem::Molecule molecule, std::vector<PseudopotentialData> pp, double prec){
+    ProjectorOperator(mrchem::Molecule molecule, std::vector<PseudopotentialData> pp, double prec){
 
         this->pp = pp;
         this->prec = prec;
 
         // loop over all atoms and create projectors
-        for (int i = 0; i < molecule.getNNuclei() i++) {
-            Eigen::Vector3d pos = molecule.getNucleus(i).getPos();
+        for (int i = 0; i < molecule.getNNuclei(); i++) {
+            mrcpp::Coord<3> pos = molecule.getNuclei()[i].getCoord();
             for (int l = 0; l < pp[i].nProjectors; l++) {
                 for (int m = -l; m <= l; m++) {
                     for (int isep = 0; isep < pp[i].nsep; isep++){
-                        proj.push_back(Projector(pos, pp[i].rl[l], isep, l, m, prec));
+                        proj.push_back(ProjectorFunction(pos, pp[i].rl[l], isep, l, m, prec));
                     }
                     
                 }
@@ -34,16 +34,16 @@ public:
 
     }
 
-mrchem::Orbital apply(mrchem::Orbital phi_p) {
-    return phi_p;
-}
+// mrchem::Orbital apply(mrchem::Orbital phi_p) {
+//     return phi_p;
+// }
 
-mrchem::Orbital dagger(mrchem::Orbital phi_p) {
-    return apply(phi_p);
-}
+// mrchem::Orbital dagger(mrchem::Orbital phi_p) {
+//     return apply(phi_p);
+// }
 
-mrchem::QMOperatorVector apply(std::shared_ptr<mrchem::QMOperator> &O) {
-    NOT_IMPLEMENTED_ABORT;
-}
+// mrchem::QMOperatorVector apply(std::shared_ptr<mrchem::QMOperator> &O) {
+//     NOT_IMPLEMENTED_ABORT;
+// }
 
 };
