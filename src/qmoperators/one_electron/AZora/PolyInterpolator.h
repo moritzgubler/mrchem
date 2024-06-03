51d1a2154d8020283cac5e5e6eb155555703f22b
moritzgubler <moritz.gubler@gmail.com>
2024-06-03 09:15:26 +0200

    duplicate some code

--- src/qmoperators/one_electron/AZora/PolyInterpolator.h
+++ src/qmoperators/one_electron/AZora/PolyInterpolator.h
@@ -1,5 +1,42 @@
 #include <vector>
 #include <Eigen/Dense>
+#include <fstream>
+#include <string>
+#include <filesystem>
+
+/**
+ * @brief Read ZORA potential from file. Check if file exists and abort if it does not.
+ * @param path Path to the file containing the ZORA potential
+ * @param rGrid Vector containing the radial grid
+ * @param vZora Vector containing the ZORA potential
+ * @param kappa Vector containing the kappa parameter
+*/
+void readZoraPotential(const std::string path, Eigen::VectorXd &rGrid, Eigen::VectorXd &vZora, Eigen::VectorXd &kappa){
+    std::vector<double> r, v, k;
+    bool file_exists = std::filesystem::exists(path);
+    if (!file_exists) {
+        std::cerr << "File " << path << " does not exist." << std::endl;
+        std::cout << "File " << path << " does not exist." << std::endl;
+        exit(1);
+    }
+    std::ifstream file(path);
+    std::string line;
+    double r_, v_, k_;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        iss >> k_ >> r_ >> v_;
+        r.push_back(r_);
+        v.push_back(v_);
+        k.push_back(k_);
+    }
+    file.close();
+    rGrid = Eigen::Map<Eigen::VectorXd>(r.data(), r.size());
+    vZora = Eigen::Map<Eigen::VectorXd>(v.data(), v.size());
+    kappa = Eigen::Map<Eigen::VectorXd>(k.data(), k.size());
+    // The kappa function is half of what is defined in the paper Scalar 
+    // Relativistic Effects with Multiwavelets: Implementation and Benchmark
+    // it is not used in the code, only the potential is used
+}
 
 double polynomialInterpolate5(Eigen::Vector5d &x_in, Eigen::Vector5d &y_in, double x){
     double xm2 = x_in(0);
