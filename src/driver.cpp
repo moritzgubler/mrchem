5be5ea5c1555a2292636fd98b0835d461617b2a0
moritzgubler <moritz.gubler@gmail.com>
2024-11-22 17:25:26 +0100

    json mess

--- src/driver.cpp
+++ src/driver.cpp
@@ -84,6 +84,7 @@
 #include "environment/Permittivity.h"
 
 #include "pseudopotential/projectorOperator.h"
+#include "pseudopotential/pseudopotential.h"
 
 #include "properties/hirshfeld/HirshfeldPartition.h"
 
@@ -1097,6 +1098,14 @@ void driver::build_fock_operator(const json &json_fock, Molecule &mol, FockBuild
             MSG_ABORT("Invalid perturbation order");
         }
     }
+    std::cout << "before getting ppsd   " << std::endl;
+    const auto &json_pp = json_fock["pseudopotentials"];
+    std::cout << "pseudopotentials" << std::endl;
+    PseudopotentialData pp_data(json_pp[0]);
+    pp_data.print();
+
+    exit(0);
+
     std::cout << "making projector operator" << std::endl;
     std::shared_ptr<ProjectorOperator> pp = std::make_shared<ProjectorOperator>(mol, 1e-5);
     pp->setup(1e-5);
