ff543afd9b8af83cf2ce33b06dc944dfbf707fa7
moritzgubler <moritz.gubler@gmail.com>
2024-12-04 13:58:06 +0100

    improve test

--- tests/qmfunctions/spherical_harmonics.cpp
+++ tests/qmfunctions/spherical_harmonics.cpp
@@ -2,6 +2,8 @@
 #include "pseudopotential/sphericalHarmonics.h"
 #include "qmfunctions/qmfunction_utils.h"
 
+#include "pseudopotential/projector.h"
+
 
 
 using namespace mrchem;
@@ -14,102 +16,36 @@ TEST_CASE("sperical_harmonics", "[spherical_harmonics]") {
 
     SECTION("calc spherical harmonics") {
         double alpha = .5;
-        OrbitalVector Phi;
-        for (int l = 0; l < 5; l++) {
-            for (int m = -l; m <= l; m++) {
-                Phi.push_back(Orbital(SPIN::Paired));
-            }
-        }
-        Phi.distribute();
+        std::vector<ProjectorFunction> pps;
         int iOrb = 0;
-        for (int l = 0; l < 5; l++) {
+        for (int l = 0; l < 4; l++) {
             for (int m = -l; m <= l; m++) {
-                auto fun = [l, m, alpha](const mrcpp::Coord<3> &r) {
-                    double normr = std::sqrt(r[0] * r[0] + r[1] * r[1] + r[2] * r[2]);
-                    switch (l) {
-                        case 0:
-                            return s0(r, normr) * std::exp(-alpha * normr);
-                        case 1:
-                            switch (m) {
-                                case -1:
-                                    return s1m1(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 1);
-                                case 0:
-                                    return s10(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 1);
-                                case 1:
-                                    return s11(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 1);
-                            }
-                        case 2:
-                            switch (m) {
-                                case -2:
-                                    return s2m2(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 2);
-                                case -1:
-                                    return s2m1(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 2);
-                                case 0:
-                                    return s20(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 2);
-                                case 1:
-                                    return s21(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 2);
-                                case 2:
-                                    return s22(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 2);
-                            }
-                        case 3:
-                            switch (m) {
-                                case -3:
-                                    return s3m3(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 3);
-                                case -2:
-                                    return s3m2(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 3);
-                                case -1:
-                                    return s3m1(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 3);
-                                case 0:
-                                    return s30(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 3);
-                                case 1:                               
-                                    return s31(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 3);
-                                case 2:                               
-                                    return s32(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 3);
-                                case 3:                               
-                                    return s33(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 3);
-                            }
-                        case 4:
-                            switch (m) {
-                                case -4:
-                                    return s4m4(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 4);
-                                case -3:
-                                    return s4m3(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 4);
-                                case -2:
-                                    return s4m2(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 4);
-                                case -1:
-                                    return s4m1(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 4);
-                                case 0:
-                                    return s40(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 4);
-                                case 1:
-                                    return s41(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 4);
-                                case 2:
-                                    return s42(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 4);
-                                case 3:
-                                    return s43(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 4);
-                                case 4:
-                                    return s44(r, normr) * std::exp(-alpha * normr) / std::pow(normr, 4);
-                            }
-                    }
-                    return 0.0;
-                };
-                if (mrcpp::mpi::my_orb(Phi[iOrb])) mrcpp::cplxfunc::project(Phi[iOrb], fun, NUMBER::Real, prec);
-                iOrb++;
+                for (int idim = 0; idim < 4; idim++) {
+                    mrcpp::Coord<3> r;
+                    r[0] = 0.0;
+                    r[1] = 0.0;
+                    r[2] = 0.0;
+                    ProjectorFunction pp(r, 1.0, idim, l, m, prec);
+                    pps.push_back(pp);
+                }
             }
         }
-        Eigen::MatrixXd S(Phi.size(), Phi.size());
-        for (int i = 0; i < Phi.size(); i++) {
-            for (int j = 0; j < Phi.size(); j++) {
+
+        Eigen::MatrixXd S(pps.size(), pps.size());
+        for (int i = 0; i < pps.size(); i++) {
+            for (int j = 0; j < pps.size(); j++) {
                 // S(i, j) = qmfunction::dot(Phi[i], Phi[j]).real();
-                S(i, j) = mrcpp::cplxfunc::dot(Phi[i], Phi[j]).real();
+                S(i, j) = mrcpp::cplxfunc::dot(pps[i].projector, pps[j].projector).real();
+                std::cout << i << " " << j << " " << S(i, j) << " " << pps[i].projector.norm() << std::endl;
             }
         }
-        Eigen::MatrixXd S_ref = Eigen::MatrixXd::Zero(Phi.size(), Phi.size());
-        for (int i = 0; i < Phi.size(); i++) {
-            S_ref(i, i) = 1.0 / (4.0 * alpha*alpha*alpha);
+        Eigen::MatrixXd S_ref = Eigen::MatrixXd::Zero(pps.size(), pps.size());
+        for (int i = 0; i < pps.size(); i++) {
+            S_ref(i, i) = 1.0;
         }
         int iorb = 0;
         int jorb = 0;
-        for (int l = 0; l < 5; l++) {
+        for (int l = 0; l < 4; l++) {
             for (int m = -l; m <= l; m++) {
                 jorb = 0;
                 for (int ll = 0; ll < 5; ll++) {
